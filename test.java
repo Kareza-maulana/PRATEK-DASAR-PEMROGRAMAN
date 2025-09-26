218d45ef68f6b030248dd1d273b39645603a1b0d
kareza-maulana <karezamaulana@gmail.com>
2025-09-26 16:57:35 +0700

    tambah scanner

--- test.java
+++ test.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 public class test{
+    Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
         System.out.println("halo dairu");
         System.out.println("halo kareza");
+
+
     }
 }
